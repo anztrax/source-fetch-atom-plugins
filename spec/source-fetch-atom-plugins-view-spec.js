b9649861a2dcb16f74eec5d102b89ad365424222
andrew ananta <kuro.ketanitem@gmail.com>
2016-12-08 12:03:08 +0700

    initial commit

---
+++ spec/source-fetch-atom-plugins-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SourceFetchAtomPluginsView from '../lib/source-fetch-atom-plugins-view';
+
+describe('SourceFetchAtomPluginsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
